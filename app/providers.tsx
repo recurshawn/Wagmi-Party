b04111740459051e8d01da4aa71794e99bdc1ab4
Shawn Pereira <shawnpereira1999@gmail.com>
2024-12-08 00:40:39 +0530

    chore: apply Huddle providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,27 @@
-'use client';
- 
-import type { ReactNode } from 'react';
-import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { base } from 'wagmi/chains'; // add baseSepolia for testing
- 
+"use client";
+
+import type { ReactNode } from "react";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import { base } from "wagmi/chains"; // add baseSepolia for testing
+
+import { HuddleProvider, HuddleClient } from "@huddle01/react";
+
+const huddleClient = new HuddleClient({
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID ?? "",
+  options: {
+    activeSpeakers: {
+      size: 8,
+    },
+  },
+});
+
 export function Providers(props: { children: ReactNode }) {
   return (
     <OnchainKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
       chain={base} // add baseSepolia for testing
     >
-      {props.children}
+      <HuddleProvider client={huddleClient}>{props.children}</HuddleProvider>
     </OnchainKitProvider>
   );
 }
